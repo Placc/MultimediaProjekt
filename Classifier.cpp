0a3fec09b615ac8919da367e54d1b65db5e88c74
MarcusHoldinghausen <marcusholdinghausen@web.de>
2014-05-28 16:58:25 +0200

    test

--- Classifier.cpp
+++ Classifier.cpp
@@ -4,6 +4,7 @@ using namespace cv;
 using namespace std;
 
 SparseVector convertToSparseVector(Mat hog_features){
+	cout << "asdf" << endl;
 	SparseVector vec(hog_features.rows * hog_features.cols);
 	for(int y = 0; y < hog_features.rows; y++){
 		for(int x = 0; x < hog_features.cols; x++){
