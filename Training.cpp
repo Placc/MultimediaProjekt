6d442deee2064e96bf2b57483ab1e22815691d35
MarcusHoldinghausen <marcusholdinghausen@web.de>
2014-06-12 16:46:04 +0200

    asdfff

--- Training.cpp
+++ Training.cpp
@@ -10,6 +10,7 @@ namespace Training {
 		vector<string> neg_examples;
 		Classifier classifier = Classifier();
 		RNG random_index = RNG( time (NULL) );
+
 		Rect roi(Point(16,16), Point(80, 144));
 		Preprocessing::loadPathsByDirectory(TRAIN_POS_NORMALIZED, pos_examples);
 		Preprocessing::loadPathsByDirectory(TRAIN_NEG_ORIGINAL, neg_examples);
@@ -35,18 +36,31 @@ namespace Training {
 				
 			}
 
-			
-			
-			
-
-			for( int i = 0; i < MAXRANDOMELEMENTS; ++i )
-			{
-				
-				classifier.addNegativeExample( img.slidingWindows[ random_index.uniform(0, img.slidingWindows.size()) ].hog_features );
+			int count = 0;
+			double howMuch = ((double)(MAXRANDOMELEMENTS)/(double)(img.lower_images.size() + 1));
+			for(int i = 0; i < (int)(howMuch); i++){
+				classifier.addNegativeExample( img.slidingWindows[ random_index.uniform( 0, img.slidingWindows.size() ) ].hog_features );
+				count++;
 			}
-
+			howMuch = howMuch + ((double)(MAXRANDOMELEMENTS)/(double)(img.lower_images.size() + 1)) - (int)howMuch;
+			if(howMuch < MAXRANDOMELEMENTS)
+				for(int current = 0; current < img.lower_images.size(); current++){
+					for(int i = 0; i < (int)(howMuch); i++){
+						int y = random_index.uniform( 0, img.lower_images[ current ].slidingWindows.size() );
+						Mat hog_features = img.lower_images[ current ].slidingWindows[ y ].hog_features;
+						classifier.addNegativeExample( hog_features );
+						count++;
+					}
+					howMuch = howMuch + ((double)(MAXRANDOMELEMENTS)/(double)(img.lower_images.size() + 1)) - (int)howMuch;
+				}
+				while(count < MAXRANDOMELEMENTS){
+					int x = random_index.uniform( 0, img.lower_images.size() );
+					int y = random_index.uniform( 0, img.lower_images[ x ].slidingWindows.size() );
+					Mat hog_features = img.lower_images[ x ].slidingWindows[ y ].hog_features;
+					classifier.addNegativeExample( hog_features );
+					count++;
+				}
 				
-
 		}
 
 		//Sliding Window HOG-Features are in OriginalImage.slidingWindows and OriginalImage.lower_images[x].slidingWindows
